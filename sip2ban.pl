490f3c0f6d8e0626e4b949be1e51f229a327f11a
Chanon <chanon_m@hotmail.com>
2015-03-18 18:49:11 -0700

    Update sip2ban.pl

--- sip2ban.pl
+++ sip2ban.pl
@@ -39,44 +39,45 @@ foreach my $item (keys %seen) {
    }
 }
 
-#read iptables configuration file
-open($fh, '<',"/etc/sysconfig/iptables") or die "Could not open file!";
-my @lines=<$fh>;
-my $linenum = scalar(@lines) - 2;
-close $fh;
+if($blockedip[0] ne "") {
+   #read iptables configuration file
+   open($fh, '<',"/etc/sysconfig/iptables") or die "Could not open file!";
+   my @lines=<$fh>;
+   my $linenum = scalar(@lines) - 2;
+   close $fh;
 
-#backup iptables configuration file
-move("/etc/sysconfig/iptables","/etc/sysconfig/iptables.old");
+   #backup iptables configuration file
+   move("/etc/sysconfig/iptables","/etc/sysconfig/iptables.old");
 
-#save and apply new iptables rules
-open($fh, '>',"/etc/sysconfig/iptables") or die "Could not open file!";
-$i = 0;
-foreach my $line (@lines) {
-
-   for(my $j=0; $j < $countip; $j++) {
-       my $str = "-A RH-Firewall-1-INPUT -s $blockedip[$j] -j DROP\n";
-       if($line eq $str) {
-         $blockedip[$j]="";
-       }
-   }
-
-   if($i == $linenum) {
+   #save and apply new iptables rules
+   open($fh, '>',"/etc/sysconfig/iptables") or die "Could not open file!";
+   $i = 0;
+   foreach my $line (@lines) {
       for(my $j=0; $j < $countip; $j++) {
-        if($blockedip[$j] ne "") {
-          #update an iptables rule in iptables configuration file
-          print $fh "-A RH-Firewall-1-INPUT -s $blockedip[$j] -j DROP\n";
-          #apply an iptables rule
-          my $returncode = system("/sbin/iptables -I RH-Firewall-1-INPUT 2 -s $blockedip[$j] -j DROP");
-          if($returncode != 0) {
-              print "Could not add $heckip[$j] in iptables rule!\n";
-          } else {
-              print "Bloacked IP Address : $heckip[$j]\n";          
-          }
-        }
+         my $str = "-A RH-Firewall-1-INPUT -s $blockedip[$j] -j DROP\n";
+         if($line eq $str) {
+           $blockedip[$j]="";
+         }
       }
-      print $fh $line;
-   } else {
-       print $fh $line;
+
+      if($i == $linenum) {
+         for(my $j=0; $j < $countip; $j++) {
+           if($blockedip[$j] ne "") {
+             #update an iptables rule in iptables configuration file
+             print $fh "-A RH-Firewall-1-INPUT -s $blockedip[$j] -j DROP\n";
+             #apply an iptables rule
+             my $returncode = system("/sbin/iptables -I RH-Firewall-1-INPUT 2 -s $blockedip[$j] -j DROP");
+             if($returncode != 0) {
+                 print "Could not add $heckip[$j] in iptables rule!\n";
+             } else {
+                print "Bloacked IP Address : $heckip[$j]\n";          
+             }
+           }
+         }
+         print $fh $line;
+      } else {
+         print $fh $line;
+      }
+      $i++;
    }
-   $i++;
 }
