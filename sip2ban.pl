63a5a3670699b5ff77ef5fb6d8ee965b79c2beee
Chanon <chanon_m@hotmail.com>
2015-04-06 22:03:42 -0700

    Update sip2ban.pl

--- sip2ban.pl
+++ sip2ban.pl
@@ -10,6 +10,7 @@ if($#ARGV != 0) {
 
 #initial variables
 my $logfile = '/var/log/asterisk/full';
+my $datetime = localtime;
 my $count = $ARGV[0];
 my $key = 'failed for';
 my $i = 0;
@@ -21,7 +22,7 @@ while (my $row = <$fh>) {
   chomp $row;
   if(index($row, $key) != -1) {
       my @data = split /[',:, ]/, $row;
-      $ip[$i++] = $data[19];
+      $ip[$i++] = $data[20];
   }
 }
 close $fh;
@@ -61,21 +62,22 @@ if($countip > 0) {
          }
       }
 
-      if($i == $linenum) {
+      my $search = "-A RH-Firewall-1-INPUT -i lo -j ACCEPT";
+      if($line =~ /$search/) {
          for(my $j=0; $j < $countip; $j++) {
+           print $fh $line;
            if($blockedip[$j] ne "") {
              #update an iptables rule in iptables configuration file
              print $fh "-A RH-Firewall-1-INPUT -s $blockedip[$j] -j DROP\n";
              #apply an iptables rule
              my $returncode = system("/sbin/iptables -I RH-Firewall-1-INPUT 2 -s $blockedip[$j] -j DROP");
              if($returncode != 0) {
-                 print "Could not add $heckip[$j] in iptables rule!\n";
+                 print "$datetime Could not add $blockedip[$j] in iptables rule!\n";
              } else {
-                print "Bloacked IP Address : $heckip[$j]\n";          
+                print "$datetime Bloacked IP Address : $blockedip[$j]\n";          
              }
            }
          }
-         print $fh $line;
       } else {
          print $fh $line;
       }
